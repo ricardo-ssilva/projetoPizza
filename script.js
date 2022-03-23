878179cd759626d202a44065d1488712c1936bb1
Ricardo Silva <ricardo.ssilva197@gmail.com>
2022-03-23 00:09:25 -0300

    Inicia o clone do projeto pizza

---
+++ script.js
@@ -0,0 +1,14 @@
+let c = (el) => document.querySelector(el)
+
+pizzaJson.map((item , index) => {
+   let pizzaItem = c('.models .pizza-item').cloneNode(true)
+  
+
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    document.querySelector('.pizza-area').append(pizzaItem)
+
+   
+})
